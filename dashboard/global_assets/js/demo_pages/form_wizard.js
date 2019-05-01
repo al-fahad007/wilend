27565ab5c0ad12e01c7ebbe02328b5552a3964ba
al_fahad <abdullahalfahad.bd@gmail.com>
2019-05-01 18:56:51 +0600

    user and admin dashboard added

--- dashboard/global_assets/js/demo_pages/form_wizard.js
+++ dashboard/global_assets/js/demo_pages/form_wizard.js
@@ -33,10 +33,7 @@ var FormWizard = function() {
             labels: {
                 previous: '<i class="icon-arrow-left13 mr-2" /> Previous',
                 next: 'Next <i class="icon-arrow-right14 ml-2" />',
-                finish: 'Submit form <i class="icon-arrow-right14 ml-2" />'
-            },
-            onFinished: function (event, currentIndex) {
-                alert('Form submitted.');
+                finish: 'Done'
             }
         });
 
